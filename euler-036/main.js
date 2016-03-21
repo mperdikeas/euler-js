50b2d7c1587fe3ea60610bbec49e9f105da7b416
mperdikeas <mperdikeas@gmail.com>
2016-03-21 18:47:53 +0200

    euler

--- euler-036/main.js
+++ euler-036/main.js
@@ -1,3 +1,4 @@
+'use strict';
 console.log(`
 The decimal number, 585(10) = 1001001001(2) (in binary), is palindromic in both bases.
 
@@ -5,3 +6,60 @@ Find the sum of all numbers, less than one million, which are palindromic in bas
 
 (Please note that the palindromic number, in either base, may not include leading zeros.)
 `);
+
+const _      = require('lodash');
+const assert = require('assert');
+
+var isPallindromic = (function() {
+    function isPallindromic(arr) {
+        let copy = arr.slice();
+        copy.reverse();
+        return _.isEqual(arr, copy);
+    }
+    assert.equal(true, isPallindromic([1,2,3,2,1]));
+    assert.equal(true, isPallindromic('abcddcba'.split('')));
+    assert.equal(false, isPallindromic([1,2,3,2,1,3]));
+    return isPallindromic;
+})();
+
+
+var pow = (function() {
+    function pow(a, n) {
+        return _.times(n, _.constant(a)).reduce ( (v,e)=>v*e, 1);
+    }
+    assert.equal(8, pow(2, 3));
+    assert.equal(9, pow(3, 2));
+    assert.equal(0, pow(0, 1));
+    assert.equal(1, pow(1, 0));
+    return pow;
+})();
+
+var digits2Number = (function() {
+    function digits2Number(digits) {
+        let sum = 0;
+        for (let i = 0 ; i < digits.length; i++) {
+            sum += digits[digits.length-1-i]*pow(10, i);
+        }
+        return sum;
+    }
+    for (let i = 0 ; i < 1000 ; i++)
+        assert.equal(i, digits2Number(i.toFixed().split('')));
+    return digits2Number;
+})();
+
+function digitsBinary(digitsDecimal) {
+    let n = digits2Number(digitsDecimal);
+    return n.toString(2).split('');
+}
+
+var isPalindromicInBase10andBase2 = (function() {
+    function isPalindromicInBase10andBase2(n){
+        return isPallindromic(n.toFixed().split(''))
+            && isPallindromic(digitsBinary(n.toFixed().split('')));
+    }
+    assert.equal(true, isPalindromicInBase10andBase2(585));
+    return isPalindromicInBase10andBase2;
+})();
+
+let answer = _.range(1, pow(10, 6)-1).filter(isPalindromicInBase10andBase2).reduce( (v,e)=>v+e, 0);
+console.log(`The answer is: ${answer}`);
--- euler-037/main.js
+++ euler-037/main.js
@@ -1,3 +1,4 @@
+'use strict';
 console.log(`
 The number 3797 has an interesting property. Being prime itself, it is possible to continuously remove digits from left to right, and remain prime at each stage: 3797, 797, 97, and 7. Similarly we can work from right to left: 3797, 379, 37, and 3.
 
@@ -5,3 +6,222 @@ Find the sum of the only eleven primes that are both truncatable from left to ri
 
 NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
 `);
+
+
+
+const _       = require('lodash');
+const assert  = require('assert');
+
+
+/***********************************************************************
+ *                                                                     *
+ *    Utility functions common to both solutions                       *
+ *                                                                     *
+ ***********************************************************************/
+
+var pow = (function() {
+    function pow(a, n) {
+        return _.times(n, _.constant(a)).reduce( (v,e)=>v*e, 1);
+    }
+    assert.equal(8, pow(2,3));
+    assert.equal(9, pow(3,2));
+    return pow;
+})();
+
+
+function digitsToNumber(digits) {
+    let sum = 0;
+    for (let i = 0; i < digits.length ; i++)
+        sum += digits[i]*pow(10, digits.length-1-i);
+    return sum;
+}
+
+
+// returns the truncations of a number
+let truncations = (function() {
+
+    function truncations(n) {
+        let rv = [];
+        function _push(v) {
+            if (!_.some(rv, x=> _.isEqual(x, v)))
+                rv.push(v);
+        }
+        let digits = n.toFixed().split('');
+        for (let i = 0; i < digits.length ; i++) {
+            _push(digits.slice(i));
+            _push(digits.slice(0, digits.length-i));
+        }
+        return rv.map(digitsToNumber);
+    };
+    return truncations;
+})();
+
+
+function solutionWithSieveOfEratosthenes(EXPECTED_NUMBER) {
+
+    var sieve = (function() {
+
+        function sieve(from, upTo) {
+            if (from<2)
+                throw new Error(`[from] cannot be lower than 2 and was: ${from}`);
+            let rv = _.range(from, upTo+1).map( (x)=>({v:x, present: true}) );
+            for (let i = 2; i <= upTo/2 ; i++) {
+                for (let j = 2; ; j++) {
+                    if (i*j > upTo)
+                        break;
+                    if (i*j < from)
+                        continue;
+                    else {
+                        let idx = i*j-from;
+                        rv[idx].present = false;
+                    }
+                }
+            }
+            let rvv = rv.filter(x=>x.present).map(x=>x.v);
+            return rvv;
+        }
+        
+        return sieve;
+    })();
+
+
+    // assumes the array is sorted
+    var binarySearch = (function() {
+        function binarySearch(arr, n) {
+
+            return binarySearchInternal(arr, 0, arr.length - 1, n);
+
+            function binarySearchInternal(arr, i, j, n) {
+                if (i>j)
+                    return -1;
+                let mid = Math.floor((j+i)/2);
+                if (arr[mid]===n)
+                    return mid;
+                else if (arr[mid]<n) {
+                    return binarySearchInternal(arr, mid+1, j, n);
+                } else {
+                    return binarySearchInternal(arr, i, mid-1, n);
+                }
+            }
+            
+        }
+        var tests = [{arr: [], x: 2, v: -1},
+                     {arr: [1], x: 2, v: -1},
+                     {arr: [1], x: 1, v: 0},
+                     {arr: [1,2,100], x: 1, v: 0},
+                     {arr: [1,2,100], x: 2, v: 1},
+                     {arr: [1,2,100], x:100, v: 2},
+                     {arr: [1,2,100], x:0   , v: -1},
+                     {arr: [1,2,100], x:1.2 , v: -1},
+                     {arr: [1,2,100], x:3   , v: -1},
+                     {arr: [1,2,100], x:101 , v: -1},
+                     {arr: _.range(1, 1000), x: 999.5, v:-1}
+                    ];
+        tests.forEach( e => {
+            assert.equal(e.v, binarySearch(e.arr, e.x));
+        });
+        return binarySearch;
+    })();
+
+    function isPrime(n, sieve) {
+        let idx = binarySearch(sieve, n);
+        let v = idx!=-1;
+        return v;
+    }
+
+
+
+    let isTruncatablePrime = (function() {
+        function isTruncatablePrime(n, sieve) {
+            if (n<10)
+                return false;
+            return _.every(truncations(n), x=>isPrime(x, sieve));
+        }
+        assert.equal(true, isTruncatablePrime(3797, sieve(2, 5000)));
+        return isTruncatablePrime;
+    })();
+
+
+    const FOUND_EM = {v: null};
+    try {
+        const SIEVE_INCREMENTS = 100000;
+        let sieveV = [];
+        let truncatablePrimes = [];
+        for (let i = 0; ; i++) {
+            let from = Math.max(     i*SIEVE_INCREMENTS+1, 2);
+            let upTo =           (i+1)*SIEVE_INCREMENTS;
+            let newSieve = sieve(from, upTo);
+            sieveV = sieveV.concat(newSieve);
+            newSieve.forEach( j => {
+                if (isTruncatablePrime(j, sieveV)) {
+                    truncatablePrimes.push(j);
+                    if (truncatablePrimes.length===EXPECTED_NUMBER) {
+                        FOUND_EM.v = truncatablePrimes;
+                        throw FOUND_EM;
+                    }
+                }
+            });
+        }
+        throw new Error('impossible');
+    } catch (e) {
+        if (e===FOUND_EM) {
+            return e.v;
+        } else {
+            throw e;
+        }
+    }
+}
+
+
+function dumbSolution(EXPECTED_NUMBER) {
+
+    function isPrime(n) {
+        if (n===1)
+            return false;
+        const UP_TO = Math.floor(Math.sqrt(n));
+        let v =  !_.range(2, UP_TO+1).some( x => n%x === 0 );
+        return v;
+    }
+
+    var isTruncatablePrime = (function() {
+        function isTruncatablePrime(n) {
+            if (n<10)
+                return false;
+            return _.every(truncations(n), isPrime);
+        }
+        assert.equal(true, isTruncatablePrime(3797));
+        return isTruncatablePrime;
+    })();
+
+    
+    let n = 10;
+    let v = [];
+    while (true) {
+        if (isTruncatablePrime(n++)) {
+            v.push(n-1);
+        }
+        if (v.length === EXPECTED_NUMBER) {
+            return v;
+        }
+    }
+}
+
+// Idea: I don't get it why only eleven (11) of them exist but since I am told that this is so,
+// I'll simply continue till I find those eleven and then sum them up.
+
+const EXPECTED_NUMBER = 11;
+(function() {
+    let start = new Date().getTime();
+    let truncatablePrimes = solutionWithSieveOfEratosthenes(EXPECTED_NUMBER);
+    let end = new Date().getTime();
+    let answer = truncatablePrimes.reduce ( (v,e)=>v+e, 0);
+    console.log(`Answer with sieve method is: ${answer} (computed in ${end-start} milliseconds)`);
+})();
+
+(function() {
+    let start = new Date().getTime();
+    let truncatablePrimes = dumbSolution(EXPECTED_NUMBER);
+    let end = new Date().getTime();
+    let answer = truncatablePrimes.reduce ( (v,e)=>v+e, 0);
+    console.log(`Answer with dumb method is: ${answer} (computed in ${end-start} milliseconds)`);
+})();
